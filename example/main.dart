0e9519f7ed4eb444b6d4d5128390bce5767fe444
Thura Aung <thura.ucsy@gmail.com>
2019-06-13 11:15:18 +0900

    test text for normalizeZawgyi make meaningful

--- example/main.dart
+++ example/main.dart
@@ -3,7 +3,7 @@ import 'package:zawgyi_converter/zawgyi_converter.dart';
 void main() {
   ZawgyiConverter zawgyiConverter = ZawgyiConverter();
   // String result = zawgyiConverter.unicodeToZawgyi("ကျွန်တော့်နာမည်သူရအောင်ပါ");
-  String result = zawgyiConverter.zawgyiToUnicode('ေနကာင္းလား');
-  // String result = zawgyiConverter.normalizeZawgyi('ေနကာင္းလား');
+  // String result = zawgyiConverter.zawgyiToUnicode('ကၽြန္ေတာ္၊ ကၽြန္မ');
+  String result = zawgyiConverter.normalizeZawgyi('ဖရံုသီး');
   print('zawgyiConverter ${result}');
 }
